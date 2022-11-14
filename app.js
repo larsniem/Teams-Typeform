7b57caa6213af8b0c8d6aa8429db386ab7787616
Lars <niemczik@dik.tu-darmstadt.de>
2022-11-14 09:42:40 +0100

    Default Port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const fs = require("fs")
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
 const validDomain = process.env.VALIDDOMAIN || `localhost:${port}`;
 const tabName = process.env.TABNAME || "My Tab";
